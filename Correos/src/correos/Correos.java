f6ca893b47796cc3cf68d2d602f019decbcee104
stephaniediazv@hotmail.com <>
2014-11-03 13:29:28 -0600

    Prueba en Ubuntu para revision

--- Correos/src/correos/Correos.java
+++ Correos/src/correos/Correos.java
@@ -3,14 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package correos;
 
+import static correos.RadixSort.sort;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import java.util.Properties;
+import java.util.Scanner;
 
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
@@ -24,80 +25,179 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
-
-
 public class Correos {
 
-    
     public static void main(String[] args) {
+
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MPrincipal().setVisible(true);
+            }
+        });
+
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Configuracion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Configuracion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Configuracion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Configuracion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Configuracion().setVisible(true);
+            }
+        });
+
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FichasBancoGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FichasBancoGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FichasBancoGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FichasBancoGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FichasBancoGUI().setVisible(true);
+            }
+        });
+
         //iniciar la interfaz gráfica
         //new FichasBancoGUI().setVisible(true);
         //Con esto tomamos la hora y fecha del sistema 
-        Date date= new Date();
-        DateFormat hourdateFormat= new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
-        
-                
-        try{
+        Date date = new Date();
+        DateFormat hourdateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+
+        try {
             //Propinexion
-            Properties props= new Properties();
+            Properties props = new Properties();
             props.put("mail.smtp.host", "smtp.gmail.com");
             //props.setProperty("mail.smtp.host","smtp.gmail.com");
             props.setProperty("mail.smtp.starttls.enable", "true");
-            props.setProperty("mail.smtp.port","587");
-            props.setProperty("mail.smtp.user","tareaprogramada.estruct@gmail.com");
+            props.setProperty("mail.smtp.port", "587");
+            props.setProperty("mail.smtp.user", "tareaprogramada.estruct@gmail.com");
             props.setProperty("mail.smtp.auth", "true");
-            
-            Session session=Session.getDefaultInstance(props, null);
+
+            Session session = Session.getDefaultInstance(props, null);
             // session.setDebug(true);
             //Preparamos la sesion
             //Session session=Session.getDefaultInstance(props);
-            BodyPart texto=new  MimeBodyPart();
-            texto.setText("Nombre del cliente: "+""+"\n"+
-                    " Tipo de cliente: "+""+"\n"+
-                    " fecha y hora: "+date);
-            
+            BodyPart texto = new MimeBodyPart();
+            texto.setText("Nombre del cliente: " + "" + "\n"
+                    + " Tipo de cliente: " + "" + "\n"
+                    + " fecha y hora: " + date);
+
             //se compone el adjunto con la imagen
-            BodyPart adjunto= new MimeBodyPart();
+            BodyPart adjunto = new MimeBodyPart();
             adjunto.setDataHandler(
-                new DataHandler(new FileDataSource("C:\\Users\\Stephanie\\Pictures\\neko rin len.jpg")));
+                    new DataHandler(new FileDataSource("/home/ruben/NetBeansProjects/ticketsbanco/Correos/src/correos/neko rin len.jpg")));
             adjunto.setFileName("neko rin len.jpg");
-            
+
             //Una multiparte para agrupar texto e imagen
-            MimeMultipart multiParte=new MimeMultipart();
+            MimeMultipart multiParte = new MimeMultipart();
             multiParte.addBodyPart(texto);
             multiParte.addBodyPart(adjunto);
-            
-            
+
             //se compone el correo,dando el de, para, asunto
             //y el contenido.
             //Construimos el mensaje
-            MimeMessage message=new MimeMessage(session);
+            MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress("stephaniediazv@gmail.com"));
             message.addRecipient(Message.RecipientType.TO,
                     new InternetAddress("stephaniediazv@hotmail.com"));
             message.setSubject("Ficha del Banco");
             //message.setText(
-              //  "mensajito con java mail<br>"+"de los buenos"+" porque si",
-               //         "ISO-8859-1","html");
+            //  "mensajito con java mail<br>"+"de los buenos"+" porque si",
+            //         "ISO-8859-1","html");
             message.setContent(multiParte);
-            
-           //lo enviamos
-            Transport t= session.getTransport("smtp");
+
+            //lo enviamos
+            Transport t = session.getTransport("smtp");
             t.connect("tareaprogramada.estruct@gmail.com", "tareaprogra");
             t.sendMessage(message, message.getAllRecipients());
-            
+
             //cierre
             t.close();
-       }
-       catch (Exception e)
-       {
-                e.printStackTrace();
-                
-                
-      }
-    }
-            
-}
-   
-    
+        } catch (Exception e) {
+            e.printStackTrace();
 
+        }
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Radix sort Test\n");
+        int n, i;
+        System.out.println("ingrese los numeros ");
+        n = scan.nextInt();
+        int arr[] = new int[n];
+        System.out.println("\nIngrese" + n + "Elementos de tipo Entero");
+        for (i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        sort(arr);
+        System.out.println("\nElementos antes de ser ordenados");
+        for (i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+            System.out.println();
+
+        }
+
+    }
+
+}
